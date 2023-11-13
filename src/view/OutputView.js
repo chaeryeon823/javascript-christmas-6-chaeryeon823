8f0b3d25e13d27b7082a7b2794ad0e425ea14762
chaeryeon823 <chaechae823@naver.com>
2023-11-13 23:34:09 +0900

    feat: 입력받은 날짜 출력

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,9 +1,12 @@
-import { Console } from "@woowacourse/mission-utils";
-import { OUTPUT_MESSAGE } from "../constant/Log.js";
+import {Console} from "@woowacourse/mission-utils";
+import {OUTPUT_MESSAGE} from "../constant/Log.js";
 
 const OutputView = {
   printStart() {
     Console.print(OUTPUT_MESSAGE.START);
+  },
+  printDatePreview(date) {
+    Console.print("12월 " + date + "일" + OUTPUT_MESSAGE.DATE_PREVIEW);
   },
   printMenu() {
     Console.print("<주문 메뉴>");
